985a85dd50f72ad6d986ff9f4ef3410e3bccbbaa
Tim Rexer <tjrexer@gmail.com>
2020-09-11 22:06:14 -0400

    updated gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,73 +6,75 @@ import webpack from 'webpack';
 import sass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import concat from 'gulp-concat';
-import runSequence from 'run-sequence';
 import pkg from './package.json';
 import webpackConfig from './webpack.config';
 import exampleWebpackConfig from './example/webpack.config.babel';
 
-gulp.task('build:lib:clean', () => {
-  del.sync(['lib', 'dist']);
+require('regenerator-runtime/runtime')
+
+gulp.task('build:lib:clean', async function(){
+  return del.sync(['lib', 'dist']);
 });
 
-gulp.task('build:lib:babel', () => gulp
-  .src(['src/**/*.js'])
-  .pipe(babel())
-  .pipe(gulp.dest('lib')));
-
-gulp.task('build:lib:umd', () => gulp
-  .src(['src/index.js'])
-  .pipe(webpackStream(webpackConfig, webpack))
-  .pipe(gulp.dest('dist')));
-
-gulp.task('build:lib:style', () => gulp
-  .src(['src/**/*.scss', '!src/**/_*.scss'])
-  .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-  .pipe(gulp.dest('lib'))
-  .pipe(concat(`${pkg.name}.css`))
-  .pipe(postcss())
-  .pipe(gulp.dest('dist')));
-
-gulp.task('build:lib:copy', () => gulp
-  .src(['src/**/*', '!src/**/*.{scss,js}'])
-  .pipe(gulp.dest('lib'))
-  .pipe(gulp.dest('dist')));
-
-gulp.task('build:lib', (callback) => {
-  runSequence(
-    'build:lib:clean',
-    'build:lib:babel',
-    'build:lib:umd',
-    'build:lib:style',
-    'build:lib:copy',
-    callback
-  );
+gulp.task('build:lib:babel', async function(){ 
+  gulp.src(['src/**/*.js'])
+    .pipe(babel())
+    .pipe(gulp.dest('lib'))
 });
 
-gulp.task('build:example:clean', () => {
-  del.sync(['example/dist']);
+gulp.task('build:lib:umd', async function(){ 
+  gulp.src(['src/index.js'])
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.task('build:example:webpack', () => gulp
-  .src(['example/app/app.js'])
-  .pipe(webpackStream(exampleWebpackConfig, webpack))
-  .pipe(gulp.dest('example/dist')));
+gulp.task('build:lib:style', async function(){ 
+  gulp.src(['src/**/*.scss', '!src/**/_*.scss'])
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('lib'))
+    .pipe(concat(`${pkg.name}.css`))
+    .pipe(postcss())
+    .pipe(gulp.dest('dist'))
+});
 
-gulp.task('build:example:copy', () => gulp
-  .src(['example/app/*', '!example/app/*.{html,js}'], { nodir: true })
-  .pipe(gulp.dest('example/dist')));
+gulp.task('build:lib:copy', async function(){ 
+  gulp.src(['src/**/*', '!src/**/*.{scss,js}'])
+    .pipe(gulp.dest('lib'))
+    .pipe(gulp.dest('dist'))
+});
 
-gulp.task('build:example', (callback) => {
-  runSequence(
+gulp.task('build:lib', gulp.series(
+  'build:lib:clean',
+  'build:lib:babel',
+  'build:lib:umd',
+  'build:lib:style',
+  'build:lib:copy'
+  ));
+
+gulp.task('build:example:clean', async function(){
+  return del.sync(['example/dist']);
+});
+
+gulp.task('build:example:webpack', async function(){ 
+  gulp.src(['example/app/app.js'], {allowEmpty: true})
+    .pipe(webpackStream(exampleWebpackConfig, webpack))
+    .pipe(gulp.dest('example/dist'))
+});
+
+gulp.task('build:example:copy', async function(){ 
+  gulp.src(['example/app/*', '!example/app/*.{html,js}'], { nodir: true })
+    .pipe(gulp.dest('example/dist'))
+});
+
+gulp.task('build:example', gulp.series(
     'build:example:clean',
     'build:example:webpack',
-    'build:example:copy',
-    callback
-  );
-});
+    'build:example:copy'
+));
 
-gulp.task('build', (callback) => {
-  runSequence('build:lib', 'build:example', callback);
-});
+gulp.task('build', gulp.series(
+  'build:lib',
+  'build:example'
+  ));
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
